5fafbeea42a738f0298f50cadc7768de8a7a4fd0
Achyut Tibrewalla <achyut@Achyuts-MacBook-Pro.local>
2020-08-01 16:17:44 -0400

    Singleton Scope

--- SpringFramework/src/main/java/com/springbasics/SpringFramework/BinarySearchImpl.java
+++ SpringFramework/src/main/java/com/springbasics/SpringFramework/BinarySearchImpl.java
@@ -2,9 +2,12 @@ package com.springbasics.SpringFramework;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON) //Different beans will be created
 public class BinarySearchImpl {
 
     @Autowired
--- SpringFramework/src/main/java/com/springbasics/SpringFramework/SpringFrameworkApplication.java
+++ SpringFramework/src/main/java/com/springbasics/SpringFramework/SpringFrameworkApplication.java
@@ -3,8 +3,11 @@ package com.springbasics.SpringFramework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com")
+//@ComponentScan("com.springbasics.SpringFramework")
 public class SpringFrameworkApplication {
 
 	public static void main(String[] args) {
@@ -13,6 +16,10 @@ public class SpringFrameworkApplication {
 
 		ApplicationContext applicationContext = SpringApplication.run(SpringFrameworkApplication.class, args);
 		BinarySearchImpl bs = applicationContext.getBean(BinarySearchImpl.class);
+
+		BinarySearchImpl bs1 = applicationContext.getBean(BinarySearchImpl.class);
+
+		System.out.println(bs == bs1); //  Default is singleton, returns the same bean
 
 		int result = bs.binarySearch(new int[]{1,2,3,4,5}, 5);
 		System.out.println(result);
