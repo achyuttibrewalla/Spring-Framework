bc068083678bdcdc96fc0d96dff454baf3c24c53
Achyut Tibrewalla <achyut@Achyuts-MacBook-Pro.local>
2020-08-02 17:59:35 -0400

    Post Construct Pre Destroy Annotations

--- SpringFramework/src/main/java/com/springbasics/SpringFramework/Basic/BinarySearchImpl.java
+++ SpringFramework/src/main/java/com/springbasics/SpringFramework/Basic/BinarySearchImpl.java
@@ -1,14 +1,22 @@
 package com.springbasics.SpringFramework.Basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.math.BigInteger;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //Different beans will be created
 public class BinarySearchImpl {
+
+    Logger logger = LoggerFactory.getLogger(BinarySearchImpl.class);
 
     @Autowired
     @Qualifier("QuickSort")
@@ -32,5 +40,15 @@ public class BinarySearchImpl {
 
         //Search the array
         return 3;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("Post Construct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        logger.info("Pre Destroy");
     }
 }
--- SpringFramework/src/main/java/com/springbasics/SpringFramework/Basic/QuickSortAlgorithm.java
+++ SpringFramework/src/main/java/com/springbasics/SpringFramework/Basic/QuickSortAlgorithm.java
@@ -1,11 +1,14 @@
 package com.springbasics.SpringFramework.Basic;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Primary //Primary to specify default autowiring in case there are more than one class for the same interface
 @Qualifier("QuickSort")
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class QuickSortAlgorithm implements SortAlgorithm {
 
     public int[] sort(int[] numbers) {
