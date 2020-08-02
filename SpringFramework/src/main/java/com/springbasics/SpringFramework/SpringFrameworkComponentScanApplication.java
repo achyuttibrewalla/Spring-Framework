f5343f9d16a09009e409c3e9dd5cef3a8b039339
Achyut Tibrewalla <achyut@Achyuts-MacBook-Pro.local>
2020-08-02 15:22:26 -0400

    Component Scan to add other packages to look for beans

---
+++ SpringFramework/src/main/java/com/springbasics/SpringFramework/SpringFrameworkComponentScanApplication.java
@@ -0,0 +1,34 @@
+package com.springbasics.SpringFramework;
+
+import com.spring.ComponentScan.ComponentDAO;
+import com.springbasics.SpringFramework.Basic.BinarySearchImpl;
+import com.springbasics.SpringFramework.Scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+
+@SpringBootApplication
+@ComponentScan("com.spring.ComponentScan")
+public class SpringFrameworkComponentScanApplication {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringFrameworkApplicationScope.class);
+
+	public static void main(String[] args) {
+
+		ApplicationContext applicationContext = SpringApplication.run(SpringFrameworkApplicationScope.class, args);
+
+		ComponentDAO componentDAO = applicationContext.getBean(ComponentDAO.class);
+		LOGGER.info("{}", componentDAO);
+
+		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
+		LOGGER.info("{}", personDAO);
+
+	}
+
+}
